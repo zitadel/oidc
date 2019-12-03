ce6f3182a2c9656910a75687565fb193b446f458
Livio Amstutz <livio.a@gmail.com>
2019-12-03 09:53:05 +0100

    utils

---
+++ pkg/utils/strings_test.go
@@ -0,0 +1,48 @@
+package utils
+
+import "testing"
+
+func TestContains(t *testing.T) {
+	type args struct {
+		list   []string
+		needle string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			"empty list false",
+			args{[]string{}, "needle"},
+			false,
+		},
+		{
+			"list not containing false",
+			args{[]string{"list"}, "needle"},
+			false,
+		},
+		{
+			"list not containing empty needle false",
+			args{[]string{"list", "needle"}, ""},
+			false,
+		},
+		{
+			"list containing true",
+			args{[]string{"list", "needle"}, "needle"},
+			true,
+		},
+		{
+			"list containing empty needle true",
+			args{[]string{"list", "needle", ""}, ""},
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Contains(tt.args.list, tt.args.needle); got != tt.want {
+				t.Errorf("Contains() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
