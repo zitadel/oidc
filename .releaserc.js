a842b13d25d5e846d550d1db99baa7e071f07d8b
Florian Forster <florian@caos.ch>
2020-04-14 18:25:02 +0200

    chore: enhance CI process (#20)
    * chore: enhance CI process
    
    * chore: enhance ci
    
    * chore: typo

--- .releaserc.js
+++ .releaserc.js
@@ -3,9 +3,6 @@ module.exports = {
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        "@semantic-release/github",
-        ["@semantic-release/exec", {
-            "prepareCmd": "echo '::set-env name=CAOS_NEXT_VERSION::v${nextRelease.version}'"
-            }],
+        "@semantic-release/github"
     ]
   };
