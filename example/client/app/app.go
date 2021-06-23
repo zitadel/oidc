fde944128c02caab2fe9744d0256b060b0cf24b5
Livio Amstutz <livio.a@gmail.com>
2021-06-23 07:30:56 +0200

    comments

--- example/client/app/app.go
+++ example/client/app/app.go
@@ -69,12 +69,27 @@ func main() {
 		w.Write(data)
 	}
 
+	//you could also just take the access_token and id_token without calling the userinfo endpoint:
+	//
+	//marshalToken := func(w http.ResponseWriter, r *http.Request, tokens *oidc.Tokens, state string, rp rp.RelyingParty) {
+	//	data, err := json.Marshal(tokens)
+	//	if err != nil {
+	//		http.Error(w, err.Error(), http.StatusInternalServerError)
+	//		return
+	//	}
+	//	w.Write(data)
+	//}
+
 	//register the CodeExchangeHandler at the callbackPath
 	//the CodeExchangeHandler handles the auth response, creates the token request and calls the callback function
 	//with the returned tokens from the token endpoint
 	//in this example the callback function itself is wrapped by the UserinfoCallback which
 	//will call the Userinfo endpoint, check the sub and pass the info into the callback function
 	http.Handle(callbackPath, rp.CodeExchangeHandler(rp.UserinfoCallback(marshalUserinfo), provider))
+
+	//if you would use the callback without calling the userinfo endpoint, simply switch the callback handler for:
+	//
+	//http.Handle(callbackPath, rp.CodeExchangeHandler(marshalToken, provider))
 
 	lis := fmt.Sprintf("127.0.0.1:%s", port)
 	logrus.Infof("listening on http://%s/", lis)
