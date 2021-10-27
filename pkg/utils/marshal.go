d1d01b6b6b73ef9bfe15be4205bb47f8b90e02e7
wener <wenermail@gmail.com>
2021-10-28 00:44:20 +0800

    fix: allowed ConcatenateJSON with empty input

--- pkg/utils/marshal.go
+++ pkg/utils/marshal.go
@@ -29,6 +29,14 @@ func ConcatenateJSON(first, second []byte) ([]byte, error) {
 	if !bytes.HasPrefix(second, []byte{'{'}) {
 		return nil, fmt.Errorf("jws: invalid JSON %s", second)
 	}
+	// check empty
+	if len(first) == 2 {
+		return second, nil
+	}
+	if len(second) == 2 {
+		return first, nil
+	}
+
 	first[len(first)-1] = ','
 	first = append(first, second[1:]...)
 	return first, nil
