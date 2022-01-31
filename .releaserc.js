0791f0dcd109011ece6f5efb6ca1576daa965889
Livio Amstutz <livio.a@gmail.com>
2022-01-31 07:30:05 +0100

    build branch

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: ["main"],
+    branches: ["main", {name: "beta", prerelease: true}],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
