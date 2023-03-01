1eb4ee1c8e5dae9c9f3d332d617d22d42b5db68b
David Sharnoff <dsharnoff@singlestore.com>
2023-03-01 12:43:09 -0800

    auto install things for "go generate" and then clean up afterwards

--- pkg/op/mock/generate.go
+++ pkg/op/mock/generate.go
@@ -1,5 +1,6 @@
 package mock
 
+//go:generate go install github.com/golang/mock/mockgen@v1.6.0
 //go:generate mockgen -package mock -destination ./storage.mock.go github.com/zitadel/oidc/v2/pkg/op Storage
 //go:generate mockgen -package mock -destination ./authorizer.mock.go github.com/zitadel/oidc/v2/pkg/op Authorizer
 //go:generate mockgen -package mock -destination ./client.mock.go github.com/zitadel/oidc/v2/pkg/op Client
