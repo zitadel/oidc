6812435612d83f157eba4788c9ed4b8bbbf8941e
Tim Möhlmann <tim+github@zitadel.com>
2023-03-22 15:09:28 +0200

    chore: v1 branch maintainance releaser

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: ["main"],
+    branches: ["1.13.x", "main"],
     plugins: [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
