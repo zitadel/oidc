f0d46593e0aa984bb1a63fc93cb79fc73b7ab59b
David Sharnoff <dsharnoff@singlestore.com>
2023-04-13 06:37:50 -0700

    feat: rp.RefreshAccessToken() now may provide an updated IDToken (#365)

--- pkg/client/profile/jwt_profile.go
+++ pkg/client/profile/jwt_profile.go
@@ -13,7 +13,7 @@ import (
 
 // jwtProfileTokenSource implement the oauth2.TokenSource
 // it will request a token using the OAuth2 JWT Profile Grant
-// therefore sending an `assertion` by singing a JWT with the provided private key
+// therefore sending an `assertion` by signing a JWT with the provided private key
 type jwtProfileTokenSource struct {
 	clientID      string
 	audience      []string
