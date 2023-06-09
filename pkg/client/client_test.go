11c3ae3306cb0cd589aca8b46561a208735f74d3
Tim Möhlmann <tim+github@zitadel.com>
2023-06-09 13:31:28 +0200

    reproduce #406

---
+++ pkg/client/client_test.go
@@ -0,0 +1,54 @@
+package client
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDiscover(t *testing.T) {
+	type wantFields struct {
+		UILocalesSupported bool
+	}
+
+	type args struct {
+		issuer       string
+		wellKnownUrl []string
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantFields *wantFields
+		wantErr    bool
+	}{
+		{
+			name: "spotify", // https://github.com/zitadel/oidc/issues/406
+			args: args{
+				issuer: "https://accounts.spotify.com",
+			},
+			wantFields: &wantFields{
+				UILocalesSupported: true,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Discover(tt.args.issuer, http.DefaultClient, tt.args.wellKnownUrl...)
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+			if tt.wantFields == nil {
+				return
+			}
+			assert.Equal(t, tt.args.issuer, got.Issuer)
+			if tt.wantFields.UILocalesSupported {
+				assert.NotEmpty(t, got.UILocalesSupported)
+			}
+		})
+	}
+}
