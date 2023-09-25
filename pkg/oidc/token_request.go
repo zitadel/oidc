c6f6a8800df39781a143bb917c86bc2c7949e22d
Tim Möhlmann <tim+github@zitadel.com>
2023-09-25 20:07:08 +0300

    cleanup unused GrantType method

--- pkg/oidc/token_request.go
+++ pkg/oidc/token_request.go
@@ -50,16 +50,6 @@ const (
 	JWTTokenType     TokenType = "urn:ietf:params:oauth:token-type:jwt"
 )
 
-func (t GrantType) IsSupported() bool {
-	for _, gt := range AllGrantTypes {
-		if t == gt {
-			return true
-		}
-	}
-
-	return false
-}
-
 var AllTokenTypes = []TokenType{
 	AccessTokenType, RefreshTokenType, IDTokenType, JWTTokenType,
 }
