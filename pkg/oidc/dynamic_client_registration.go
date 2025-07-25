fc3b6cdcbd0d1231e1c08476f4a66371d4be027d
mqf20 <mingqingfoo@gmail.com>
2025-07-25 20:37:11 +0800

    WIP added request and response structs
    Signed-off-by: mqf20 <mingqingfoo@gmail.com>

--- pkg/oidc/dynamic_client_registration.go
+++ pkg/oidc/dynamic_client_registration.go
@@ -1,22 +1,31 @@
 package oidc
 
+import (
+	"github.com/go-jose/go-jose/v4"
+)
+
 // ClientRegistrationRequest implements
 // https://www.rfc-editor.org/rfc/rfc7591#section-3.1,
 // 3.1 Client Registration Request.
+//
+// Can also be used for https://www.rfc-editor.org/rfc/rfc7592.html#section-2.1
+// 2.2 Client Update Request.
 type ClientRegistrationRequest struct {
-	ApplicationType string   `json:"application_type"`
-	RedirectUris    []string `json:"redirect_uris"`
-	// ClientName contains a list of BCP47 language tag values that the OP supports.
-	ClientName                   Locales  `json:"client_name"`
-	LogoUri                      Locales  `json:"logo_uri"`
-	SubjectType                  string   `json:"subject_type"`
-	SectorIdentifierUri          Locales  `json:"sector_identifier_uri"`
-	TokenEndpointAuthMethod      string   `json:"token_endpoint_auth_method"`
-	JwksUri                      string   `json:"jwks_uri"`
-	UserinfoEncryptedResponseAlg string   `json:"userinfo_encrypted_response_alg"`
-	UserinfoEncryptedResponseEnc string   `json:"userinfo_encrypted_response_enc"`
-	Contacts                     []string `json:"contacts"`
-	RequestUris                  []string `json:"request_uris"`
+	RedirectURIs            []string           `json:"redirect_uris"`              // Array of redirection URI strings for use in redirect-based flows such as the authorization code and implicit flows.
+	TokenEndpointAuthMethod AuthMethod         `json:"token_endpoint_auth_method"` // String indicator of the requested authentication method for the token endpoint.
+	GrantTypes              []GrantType        `json:"grant_types"`                // Array of OAuth 2.0 grant type strings that the client can use at the token endpoint.
+	ResponseTypes           []ResponseType     `json:"response_types"`             // Array of the OAuth 2.0 response type strings that the client can use at the authorization endpoint.
+	ClientName              string             `json:"client_name"`                // Human-readable string name of the client to be presented to the end-user during authorization. (BCP 47)
+	ClientURI               string             `json:"client_uri"`                 // URL string of a web page providing information about the client. (BCP 47)
+	LogoURI                 string             `json:"logo_uri"`                   // URL string that references a logo for the client. (BCP 47)
+	Scope                   string             `json:"scope"`                      // String containing a space-separated list of scope values (as described in Section 3.3 of OAuth 2.0 [RFC6749]) that the client can use when requesting access tokens.
+	Contacts                []string           `json:"contacts"`                   // Array of strings representing ways to contact people responsible for this client, typically email addresses.
+	TOSURI                  string             `json:"tos_uri"`                    // URL string that points to a human-readable terms of service document for the client that describes a contractual relationship between the end-user and the client that the end-user accepts when authorizing the client. (BCP 47)
+	PolicyURI               string             `json:"policy_uri"`                 // URL string that points to a human-readable privacy policy document that describes how the deployment organization collects, uses, retains, and discloses personal data.
+	JWKSURI                 string             `json:"jwks_uri"`                   // URL string referencing the client's JSON Web Key (JWK) Set [RFC7517] document, which contains the client's public keys.
+	JWKS                    jose.JSONWebKeySet `json:"jwks"`                       // Client's JSON Web Key Set [RFC7517] document value, which contains the client's public keys.
+	SoftwareID              string             `json:"software_id"`                // A unique identifier string (e.g., a Universally Unique Identifier (UUID)) assigned by the client developer or software publisher used by registration endpoints to identify the client software to be dynamically registered.
+	SoftwareVersion         string             `json:"software_version"`           // A version identifier string for the client software identified by "software_id".
 }
 
 // ClientInformationResponse implements
@@ -25,29 +34,44 @@ type ClientRegistrationRequest struct {
 // https://www.rfc-editor.org/rfc/rfc7592.html#section-3
 // 3. Client Information Response.
 type ClientInformationResponse struct {
-	// TODO
+	ClientID              string `json:"client_id"`                          // OAuth 2.0 client identifier string.
+	ClientSecret          string `json:"client_secret,omitempty"`            // OAuth 2.0 client secret string.
+	ClientIDIssuedAt      int64  `json:"client_id_issued_at,omitempty"`      // Time at which the client identifier was issued.
+	ClientSecretExpiresAt int64  `json:"client_secret_expires_at,omitempty"` // Time at which the client secret will expire or 0 if it will not expire.
+
+	// fields that are reused from ClientRegistrationRequest
+	RedirectURIs            []string           `json:"redirect_uris,omitempty"`              // Array of redirection URI strings for use in redirect-based flows such as the authorization code and implicit flows.
+	TokenEndpointAuthMethod AuthMethod         `json:"token_endpoint_auth_method,omitempty"` // String indicator of the requested authentication method for the token endpoint.
+	GrantTypes              []GrantType        `json:"grant_types,omitempty"`                // Array of OAuth 2.0 grant type strings that the client can use at the token endpoint.
+	ResponseTypes           []ResponseType     `json:"response_types,omitempty"`             // Array of the OAuth 2.0 response type strings that the client can use at the authorization endpoint.
+	ClientName              string             `json:"client_name,omitempty"`                // Human-readable string name of the client to be presented to the end-user during authorization. (BCP 47)
+	ClientURI               string             `json:"client_uri,omitempty"`                 // URL string of a web page providing information about the client. (BCP 47)
+	LogoURI                 string             `json:"logo_uri,omitempty"`                   // URL string that references a logo for the client. (BCP 47)
+	Scope                   string             `json:"scope,omitempty"`                      // String containing a space-separated list of scope values (as described in Section 3.3 of OAuth 2.0 [RFC6749]) that the client can use when requesting access tokens.
+	Contacts                []string           `json:"contacts,omitempty"`                   // Array of strings representing ways to contact people responsible for this client, typically email addresses.
+	TOSURI                  string             `json:"tos_uri,omitempty"`                    // URL string that points to a human-readable terms of service document for the client that describes a contractual relationship between the end-user and the client that the end-user accepts when authorizing the client. (BCP 47)
+	PolicyURI               string             `json:"policy_uri,omitempty"`                 // URL string that points to a human-readable privacy policy document that describes how the deployment organization collects, uses, retains, and discloses personal data. (BCP 47)
+	JWKSURI                 string             `json:"jwks_uri,omitempty"`                   // URL string referencing the client's JSON Web Key (JWK) Set [RFC7517] document, which contains the client's public keys.
+	JWKS                    jose.JSONWebKeySet `json:"jwks,omitempty"`                       // Client's JSON Web Key Set [RFC7517] document value, which contains the client's public keys.
+	SoftwareID              string             `json:"software_id,omitempty"`                // A unique identifier string (e.g., a Universally Unique Identifier (UUID)) assigned by the client developer or software publisher used by registration endpoints to identify the client software to be dynamically registered.
+	SoftwareVersion         string             `json:"software_version,omitempty"`           // A version identifier string for the client software identified by "software_id".
 }
 
-// ClientReadRequest implements
-// https://www.rfc-editor.org/rfc/rfc7592.html#section-2.1,
-// 2.1 Client Read Request.
-type ClientReadRequest struct {
-	// TODO
-	ClientID string `schema:"client_id"`
+// ClientInformationErrorResponse implements
+// https://www.rfc-editor.org/rfc/rfc7591#section-3.2.1,
+// 3.2.1. Client Information Response and
+// https://www.rfc-editor.org/rfc/rfc7592.html#section-3
+// 3. Client Information Response.
+type ClientInformationErrorResponse struct {
+	Error            ClientInformationErrorResponseErrorCode `json:"error"`                       // Single ASCII error code string.
+	ErrorDescription string                                  `json:"error_description,omitempty"` // Human-readable ASCII text description of the error used for debugging.
 }
 
-// ClientUpdateRequest implements
-// https://www.rfc-editor.org/rfc/rfc7592.html#section-2.2,
-// 2.2 Client Update Request.
-type ClientUpdateRequest struct {
-	// TODO
-	ClientID string `schema:"client_id"`
-}
+const (
+	ClientInformationErrorResponseErrorCodeInvalidRedirectURI          ClientInformationErrorResponseErrorCode = "invalid_redirect_uri"          // The value of one or more redirection URIs is invalid.
+	ClientInformationErrorResponseErrorCodeInvalidClientMetadata       ClientInformationErrorResponseErrorCode = "invalid_client_metadata"       // The value of one of the client metadata fields is invalid and the server has rejected this request.
+	ClientInformationErrorResponseErrorCodeInvalidSoftwareStatement    ClientInformationErrorResponseErrorCode = "invalid_software_statement"    // The software statement presented is invalid.
+	ClientInformationErrorResponseErrorCodeUnapprovedSoftwareStatement ClientInformationErrorResponseErrorCode = "unapproved_software_statement" // The software statement presented is not approved for use by this authorization server.
+)
 
-// ClientDeleteRequest implements
-// https://www.rfc-editor.org/rfc/rfc7592.html#section-2.3,
-// 2.3 Client Delete Request.
-type ClientDeleteRequest struct {
-	// TODO
-	ClientID string `schema:"client_id"`
-}
+type ClientInformationErrorResponseErrorCode string
--- pkg/oidc/dynamic_client_registration_test.go
+++ pkg/oidc/dynamic_client_registration_test.go
@@ -2,12 +2,14 @@ package oidc
 
 import (
 	"encoding/json"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestClientRegistrationRequest(t *testing.T) {
-	t.Run("unmarshal example from https://www.rfc-editor.org/rfc/rfc7591#page-17", func(t *testing.T) {
+	// example from https://www.rfc-editor.org/rfc/rfc7591#page-17
+	t.Run("unmarshal Client Registration Request example", func(t *testing.T) {
 		marshalled := []byte(`
 {
 	"redirect_uris": [
@@ -25,8 +27,10 @@ func TestClientRegistrationRequest(t *testing.T) {
 		var req ClientRegistrationRequest
 		err := json.Unmarshal(marshalled, &req)
 		require.NoError(t, err)
+		assert.Len(t, req.RedirectURIs, 2)
 	})
-	t.Run("unmarshal example from https://www.rfc-editor.org/rfc/rfc7591#page-18", func(t *testing.T) {
+	// example from https://www.rfc-editor.org/rfc/rfc7591#page-18
+	t.Run("unmarshal Client Registration Request example", func(t *testing.T) {
 		marshalled := []byte(`
 {
     "redirect_uris": [
@@ -40,12 +44,7 @@ func TestClientRegistrationRequest(t *testing.T) {
 	"jwks": {
 		"keys": [{
 			"e": "AQAB",
-			"n": "nj3YJwsLUFl9BmpAbkOswCNVx17Eh9wMO-_AReZwBqfaWFcfG
-				HrZXsIV2VMCNVNU8Tpb4obUaSXcRcQ-VMsfQPJm9IzgtRdAY8NN8Xb7PEcYyk
-				lBjvTtuPbpzIaqyiUepzUXNDFuAOOkrIol3WmflPUUgMKULBN0EUd1fpOD70p
-				RM0rlp_gg_WNUKoW1V-3keYUJoXH9NztEDm_D2MQXj9eGOJJ8yPgGL8PAZMLe
-				2R7jb9TxOCPDED7tY_TU4nFPlxptw59A42mldEmViXsKQt60s1SLboazxFKve
-				qXC_jpLUt22OC6GUG63p-REw-ZOr3r845z50wMuzifQrMI9bQ",
+			"n": "nj3YJwsLUFl9BmpAbkOswCNVx17Eh9wMO-_AReZwBqfaWFcfGHrZXsIV2VMCNVNU8Tpb4obUaSXcRcQ-VMsfQPJm9IzgtRdAY8NN8Xb7PEcYyklBjvTtuPbpzIaqyiUepzUXNDFuAOOkrIol3WmflPUUgMKULBN0EUd1fpOD70pRM0rlp_gg_WNUKoW1V-3keYUJoXH9NztEDm_D2MQXj9eGOJJ8yPgGL8PAZMLe2R7jb9TxOCPDED7tY_TU4nFPlxptw59A42mldEmViXsKQt60s1SLboazxFKveqXC_jpLUt22OC6GUG63p-REw-ZOr3r845z50wMuzifQrMI9bQ",
 			"kty": "RSA"
 		}]
 	},
@@ -56,23 +55,15 @@ func TestClientRegistrationRequest(t *testing.T) {
 		err := json.Unmarshal(marshalled, &req)
 		require.NoError(t, err)
 	})
-	t.Run("unmarshal example from https://www.rfc-editor.org/rfc/rfc7591#page-19", func(t *testing.T) {
+	// from https://www.rfc-editor.org/rfc/rfc7591#page-19
+	t.Run("unmarshal Client Registration Request example", func(t *testing.T) {
 		marshalled := []byte(`
 {
     "redirect_uris": [
         "https://client.example.org/callback",
         "https://client.example.org/callback2"
     ],
-    "software_statement": "eyJhbGciOiJSUzI1NiJ9.
-        eyJzb2Z0d2FyZV9pZCI6IjROUkIxLTBYWkFCWkk5RTYtNVNNM1IiLCJjbGll
-        bnRfbmFtZSI6IkV4YW1wbGUgU3RhdGVtZW50LWJhc2VkIENsaWVudCIsImNs
-        aWVudF91cmkiOiJodHRwczovL2NsaWVudC5leGFtcGxlLm5ldC8ifQ.
-        GHfL4QNIrQwL18BSRdE595T9jbzqa06R9BT8w409x9oIcKaZo_mt15riEXHa
-        zdISUvDIZhtiyNrSHQ8K4TvqWxH6uJgcmoodZdPwmWRIEYbQDLqPNxREtYn0
-        5X3AR7ia4FRjQ2ojZjk5fJqJdQ-JcfxyhK-P8BAWBd6I2LLA77IG32xtbhxY
-        fHX7VhuU5ProJO8uvu3Ayv4XRhLZJY4yKfmyjiiKiPNe-Ia4SMy_d_QSWxsk
-        U5XIQl5Sa2YRPMbDRXttm2TfnZM1xx70DoYi8g6czz-CPGRi4SW_S2RKHIJf
-        IjoI3zTJ0Y2oe0_EJAiXbL6OyF9S5tKxDXV8JIndSA",
+    "software_statement": "eyJhbGciOiJSUzI1NiJ9.eyJzb2Z0d2FyZV9pZCI6IjROUkIxLTBYWkFCWkk5RTYtNVNNM1IiLCJjbGllbnRfbmFtZSI6IkV4YW1wbGUgU3RhdGVtZW50LWJhc2VkIENsaWVudCIsImNsaWVudF91cmkiOiJodHRwczovL2NsaWVudC5leGFtcGxlLm5ldC8ifQ.GHfL4QNIrQwL18BSRdE595T9jbzqa06R9BT8w409x9oIcKaZo_mt15riEXHazdISUvDIZhtiyNrSHQ8K4TvqWxH6uJgcmoodZdPwmWRIEYbQDLqPNxREtYn05X3AR7ia4FRjQ2ojZjk5fJqJdQ-JcfxyhK-P8BAWBd6I2LLA77IG32xtbhxYfHX7VhuU5ProJO8uvu3Ayv4XRhLZJY4yKfmyjiiKiPNe-Ia4SMy_d_QSWxskU5XIQl5Sa2YRPMbDRXttm2TfnZM1xx70DoYi8g6czz-CPGRi4SW_S2RKHIJfIjoI3zTJ0Y2oe0_EJAiXbL6OyF9S5tKxDXV8JIndSA",
     "scope": "read write",
     "example_extension_parameter": "example_value"
 }
@@ -80,5 +71,115 @@ func TestClientRegistrationRequest(t *testing.T) {
 		var req ClientRegistrationRequest
 		err := json.Unmarshal(marshalled, &req)
 		require.NoError(t, err)
+	})
+	// from https://www.rfc-editor.org/rfc/rfc7592.html#page-7
+	t.Run("unmarshal Client Update Request example", func(t *testing.T) {
+		marshalled := []byte(`
+{
+	"client_id": "s6BhdRkqt3",
+	"client_secret": "cf136dc3c1fc93f31185e5885805d",
+	"redirect_uris": [
+		"https://client.example.org/callback",
+		"https://client.example.org/alt"
+	],
+	"grant_types": ["authorization_code", "refresh_token"],
+	"token_endpoint_auth_method": "client_secret_basic",
+	"jwks_uri": "https://client.example.org/my_public_keys.jwks",
+	"client_name": "My New Example",
+	"client_name#fr": "Mon Nouvel Exemple",
+	"logo_uri": "https://client.example.org/newlogo.png",
+	"logo_uri#fr": "https://client.example.org/fr/newlogo.png"
+}
+`)
+		var req ClientRegistrationRequest
+		err := json.Unmarshal(marshalled, &req)
+		require.NoError(t, err)
+	})
+}
+
+func TestClientInformationResponse(t *testing.T) {
+	// example from https://www.rfc-editor.org/rfc/rfc7591#page-21
+	t.Run("unmarshal example", func(t *testing.T) {
+		marshalled := []byte(`
+{
+	"client_id": "s6BhdRkqt3",
+	"client_secret": "cf136dc3c1fc93f31185e5885805d",
+	"client_id_issued_at": 2893256800,
+	"client_secret_expires_at": 2893276800,
+	"redirect_uris": [
+		"https://client.example.org/callback",
+		"https://client.example.org/callback2"
+	],
+	"grant_types": ["authorization_code", "refresh_token"],
+	"client_name": "My Example Client",
+	"client_name#ja-Jpan-JP": "\u30AF\u30E9\u30A4\u30A2\u30F3\u30C8\u540D",
+	"token_endpoint_auth_method": "client_secret_basic",
+	"logo_uri": "https://client.example.org/logo.png",
+	"jwks_uri": "https://client.example.org/my_public_keys.jwks",
+	"example_extension_parameter": "example_value"
+}
+`)
+		var req ClientInformationResponse
+		err := json.Unmarshal(marshalled, &req)
+		require.NoError(t, err)
+	})
+
+	// example fromhttps://www.rfc-editor.org/rfc/rfc7592.html#page-11
+	t.Run("unmarshal example", func(t *testing.T) {
+		marshalled := []byte(`
+{
+	"registration_access_token": "reg-23410913-abewfq.123483",
+	"registration_client_uri":
+	 "https://server.example.com/register/s6BhdRkqt3",
+	"client_id": "s6BhdRkqt3",
+	"client_secret": "cf136dc3c1fc93f31185e5885805d",
+	"client_id_issued_at": 2893256800,
+	"client_secret_expires_at": 2893276800,
+	"client_name": "My Example Client",
+	"client_name#ja-Jpan-JP": "\u30AF\u30E9\u30A4\u30A2\u30F3\u30C8\u540D",
+	"redirect_uris": [
+		"https://client.example.org/callback",
+		"https://client.example.org/callback2"
+	],
+	"grant_types": ["authorization_code", "refresh_token"],
+	"token_endpoint_auth_method": "client_secret_basic",
+	"logo_uri": "https://client.example.org/logo.png",
+	"jwks_uri": "https://client.example.org/my_public_keys.jwks"
+}
+`)
+		var req ClientInformationResponse
+		err := json.Unmarshal(marshalled, &req)
+		require.NoError(t, err)
+	})
+}
+
+func TestClientInformationErrorResponse(t *testing.T) {
+	// example from https://www.rfc-editor.org/rfc/rfc7591#page-23
+	t.Run("unmarshal example", func(t *testing.T) {
+		marshalled := []byte(`
+{
+	"error": "invalid_redirect_uri",
+	"error_description": "The redirection URI http://sketchy.example.com is not allowed by this server."
+}
+`)
+		var req ClientInformationErrorResponse
+		err := json.Unmarshal(marshalled, &req)
+		require.NoError(t, err)
+		assert.Equal(t, ClientInformationErrorResponseErrorCodeInvalidRedirectURI, req.Error)
+		assert.Equal(t, "The redirection URI http://sketchy.example.com is not allowed by this server.", req.ErrorDescription)
+	})
+	// example from https://www.rfc-editor.org/rfc/rfc7591#page-23
+	t.Run("unmarshal example", func(t *testing.T) {
+		marshalled := []byte(`
+{
+	"error": "invalid_client_metadata",
+	"error_description": "The grant type 'authorization_code' must be registered along with the response type 'code' but found only 'implicit' instead."
+}
+`)
+		var req ClientInformationErrorResponse
+		err := json.Unmarshal(marshalled, &req)
+		require.NoError(t, err)
+		assert.Equal(t, ClientInformationErrorResponseErrorCodeInvalidClientMetadata, req.Error)
+		assert.Equal(t, "The grant type 'authorization_code' must be registered along with the response type 'code' but found only 'implicit' instead.", req.ErrorDescription)
 	})
 }
