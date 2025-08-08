98ebfbc1806fcb007729d4de2423ae6cecc87253
mqf20 <mingqingfoo@gmail.com>
2025-08-08 23:10:37 +0800

    WIP added return codes
    Signed-off-by: mqf20 <mingqingfoo@gmail.com>

--- pkg/op/dynamic_client_registration.go
+++ pkg/op/dynamic_client_registration.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-jose/go-jose/v4/json"
 	httphelper "github.com/zitadel/oidc/v3/pkg/http"
 	"github.com/zitadel/oidc/v3/pkg/oidc"
+	"log/slog"
 	"net/http"
 	"strings"
 )
@@ -37,13 +38,16 @@ func getBearerToken(r *http.Request) (string, error) {
 	return strings.TrimPrefix(auth, oidc.PrefixBearer), nil
 }
 
+func clientRequestError(w http.ResponseWriter, r *http.Request, lvl slog.Level, errResp *oidc.ClientInformationErrorResponse, logger *slog.Logger, status int) {
+	logger.Log(r.Context(), lvl, "request error", "oidc_error", errResp)
+	httphelper.MarshalJSONWithStatus(w, errResp, status)
+}
+
 func clientReadHandler(o OpenIDProvider) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
-			if err := clientRead(w, r, o); err != nil {
-				RequestError(w, r, err, o.Logger())
-			}
+			clientRead(w, r, o)
 		default:
 			RequestError(w, r, fmt.Errorf("unsupported method: %s", r.Method), o.Logger())
 		}
@@ -55,39 +59,55 @@ func clientReadHandler(o OpenIDProvider) func(http.ResponseWriter, *http.Request
 //
 // [client read requests]: https://www.rfc-editor.org/rfc/rfc7592.html#section-2.1
 // [OAuth 2.0 Dynamic Client Registration Management Protocol]: https://www.rfc-editor.org/rfc/rfc7592.html
-func clientRead(w http.ResponseWriter, r *http.Request, o OpenIDProvider) error {
+func clientRead(w http.ResponseWriter, r *http.Request, o OpenIDProvider) {
 	ctx, span := tracer.Start(r.Context(), "clientRead")
 	r = r.WithContext(ctx)
 	defer span.End()
 
 	storage, err := assertClientStorage(o.Storage())
 	if err != nil {
-		return err
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	req, err := ParseClientReadRequest(r, o)
 	if err != nil {
-		// TODO(mqf20): be able to return the proper error codes?
-		return err
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	registrationAccessToken, err := getBearerToken(r)
 	if err != nil {
-		return err
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	if err := storage.AuthorizeClientRead(ctx, req.ClientID, registrationAccessToken); err != nil {
-		return err
+		if errors.Is(err, ErrInvalidClient) || errors.Is(err, ErrInvalidRegistrationAccessToken) {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		if errors.Is(err, ErrClientNoPermission) {
+			http.Error(w, err.Error(), http.StatusForbidden)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	res, err := storage.ReadClient(r.Context(), req.ClientID)
 	if err != nil {
-		// TODO(mqf20): be able to return the proper error codes?
-		return err
+		if errors.Is(err, ErrInvalidClient) {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		o.Logger().Log(r.Context(), slog.LevelError, "read client error", "error", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	httphelper.MarshalJSON(w, res)
-	return nil
+	return
 }
 
 func ParseClientReadRequest(r *http.Request, o OpenIDProvider) (*oidc.ClientReadRequest, error) {
@@ -108,17 +128,11 @@ func clientRegistrationUpdateDeleteHandler(o OpenIDProvider) func(http.ResponseW
 	return func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodPost:
-			if err := clientRegistration(w, r, o); err != nil {
-				RequestError(w, r, err, o.Logger())
-			}
+			clientRegistration(w, r, o)
 		case http.MethodPut:
-			if err := clientUpdate(w, r, o); err != nil {
-				RequestError(w, r, err, o.Logger())
-			}
+			clientUpdate(w, r, o)
 		case http.MethodDelete:
-			if err := clientDelete(w, r, o); err != nil {
-				RequestError(w, r, err, o.Logger())
-			}
+			clientDelete(w, r, o)
 		default:
 			RequestError(w, r, fmt.Errorf("unsupported method: %s", r.Method), o.Logger())
 		}
@@ -130,20 +144,21 @@ func clientRegistrationUpdateDeleteHandler(o OpenIDProvider) func(http.ResponseW
 //
 // [client registration requests]: https://www.rfc-editor.org/rfc/rfc7591#section-3.1
 // [OAuth 2.0 Dynamic Client Registration Protocol]: https://www.rfc-editor.org/rfc/rfc7591
-func clientRegistration(w http.ResponseWriter, r *http.Request, o OpenIDProvider) error {
+func clientRegistration(w http.ResponseWriter, r *http.Request, o OpenIDProvider) {
 	ctx, span := tracer.Start(r.Context(), "clientRegistration")
 	r = r.WithContext(ctx)
 	defer span.End()
 
 	storage, err := assertClientStorage(o.Storage())
 	if err != nil {
-		return err
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	req, err := ParseClientRegistrationRequest(r)
 	if err != nil {
-		// TODO(mqf20): be able to return the proper error codes?
-		return err
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	var initialAccessToken string
@@ -151,19 +166,83 @@ func clientRegistration(w http.ResponseWriter, r *http.Request, o OpenIDProvider
 		iat, err := getBearerToken(r)
 		if err != nil && !errors.Is(err, errMissingAuthorizationHeader) {
 			// allow for missing authorization header, in case the software statement is used for authentication
-			return err
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
 		}
 		initialAccessToken = iat
 	}
 
 	if err := storage.AuthorizeClientRegistration(ctx, initialAccessToken, req); err != nil {
-		return err
+		if errors.Is(err, ErrInvalidInitialAccessToken) {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		if errors.Is(err, ErrInvalidSoftwareStatement) {
+			clientRequestError(
+				w,
+				r,
+				slog.LevelInfo,
+				&oidc.ClientInformationErrorResponse{
+					Error:            oidc.ClientInformationErrorResponseErrorCodeInvalidSoftwareStatement,
+					ErrorDescription: err.Error(),
+				},
+				o.Logger(),
+				http.StatusBadRequest,
+			)
+			return
+		}
+		if errors.Is(err, ErrUnapprovedSoftwareStatement) {
+			clientRequestError(
+				w,
+				r,
+				slog.LevelInfo,
+				&oidc.ClientInformationErrorResponse{
+					Error:            oidc.ClientInformationErrorResponseErrorCodeUnapprovedSoftwareStatement,
+					ErrorDescription: err.Error(),
+				},
+				o.Logger(),
+				http.StatusBadRequest,
+			)
+			return
+		}
+		o.Logger().Log(r.Context(), slog.LevelError, "read client error", "error", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	res, err := storage.RegisterClient(ctx, req)
 	if err != nil {
-		// TODO(mqf20): be able to return the proper error codes?
-		return err
+		if errors.Is(err, ErrInvalidRedirectURI) {
+			clientRequestError(
+				w,
+				r,
+				slog.LevelInfo,
+				&oidc.ClientInformationErrorResponse{
+					Error:            oidc.ClientInformationErrorResponseErrorCodeInvalidRedirectURI,
+					ErrorDescription: err.Error(),
+				},
+				o.Logger(),
+				http.StatusBadRequest,
+			)
+			return
+		}
+		if errors.Is(err, ErrInvalidClientMetadata) {
+			clientRequestError(
+				w,
+				r,
+				slog.LevelInfo,
+				&oidc.ClientInformationErrorResponse{
+					Error:            oidc.ClientInformationErrorResponseErrorCodeInvalidClientMetadata,
+					ErrorDescription: err.Error(),
+				},
+				o.Logger(),
+				http.StatusBadRequest,
+			)
+			return
+		}
+		o.Logger().Log(r.Context(), slog.LevelError, "register client error", "error", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	// Upon a successful registration request, the authorization server
@@ -172,7 +251,7 @@ func clientRegistration(w http.ResponseWriter, r *http.Request, o OpenIDProvider
 	// containing a Client Information Response.
 
 	httphelper.MarshalJSONWithStatus(w, res, http.StatusCreated)
-	return nil
+	return
 }
 
 func ParseClientRegistrationRequest(r *http.Request) (*oidc.ClientRegistrationRequest, error) {
@@ -193,39 +272,87 @@ func ParseClientRegistrationRequest(r *http.Request) (*oidc.ClientRegistrationRe
 //
 // [client update requests]: https://www.rfc-editor.org/rfc/rfc7592.html#section-2.2
 // [OAuth 2.0 Dynamic Client Registration Management Protocol]: https://www.rfc-editor.org/rfc/rfc7592.html
-func clientUpdate(w http.ResponseWriter, r *http.Request, o OpenIDProvider) error {
+func clientUpdate(w http.ResponseWriter, r *http.Request, o OpenIDProvider) {
 	ctx, span := tracer.Start(r.Context(), "clientUpdate")
 	r = r.WithContext(ctx)
 	defer span.End()
 
 	storage, err := assertClientStorage(o.Storage())
 	if err != nil {
-		return err
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	req, err := ParseClientUpdateRequest(r, o)
 	if err != nil {
-		// TODO(mqf20): be able to return the proper error codes?
-		return err
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	registrationAccessToken, err := getBearerToken(r)
 	if err != nil {
-		return err
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	if err := storage.AuthorizeClientUpdate(ctx, req.ClientID, registrationAccessToken); err != nil {
-		return err
+		if errors.Is(err, ErrInvalidClient) || errors.Is(err, ErrInvalidRegistrationAccessToken) {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		if errors.Is(err, ErrClientNoPermission) {
+			http.Error(w, err.Error(), http.StatusForbidden)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	res, err := storage.UpdateClient(ctx, req)
 	if err != nil {
-		// TODO(mqf20): be able to return the proper error codes?
-		return err
+		if errors.Is(err, ErrInvalidClient) {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		if errors.Is(err, ErrInvalidRedirectURI) {
+			clientRequestError(
+				w,
+				r,
+				slog.LevelInfo,
+				&oidc.ClientInformationErrorResponse{
+					Error:            oidc.ClientInformationErrorResponseErrorCodeInvalidRedirectURI,
+					ErrorDescription: err.Error(),
+				},
+				o.Logger(),
+				http.StatusBadRequest,
+			)
+			return
+		}
+		if errors.Is(err, ErrInvalidClientMetadata) {
+			clientRequestError(
+				w,
+				r,
+				slog.LevelInfo,
+				&oidc.ClientInformationErrorResponse{
+					Error:            oidc.ClientInformationErrorResponseErrorCodeInvalidClientMetadata,
+					ErrorDescription: err.Error(),
+				},
+				o.Logger(),
+				http.StatusBadRequest,
+			)
+			return
+		}
+		if errors.Is(err, ErrClientUpdateNotAllowed) {
+			http.Error(w, err.Error(), http.StatusForbidden)
+			return
+		}
+		o.Logger().Log(r.Context(), slog.LevelError, "update client error", "error", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	httphelper.MarshalJSON(w, res)
-	return nil
+	return
 }
 
 func ParseClientUpdateRequest(r *http.Request, o OpenIDProvider) (*oidc.ClientUpdateRequest, error) {
@@ -246,38 +373,62 @@ func ParseClientUpdateRequest(r *http.Request, o OpenIDProvider) (*oidc.ClientUp
 //
 // [client delete requests]: https://www.rfc-editor.org/rfc/rfc7592.html#section-2.3
 // [OAuth 2.0 Dynamic Client Registration Management Protocol]: https://www.rfc-editor.org/rfc/rfc7592.html
-func clientDelete(w http.ResponseWriter, r *http.Request, o OpenIDProvider) error {
+func clientDelete(w http.ResponseWriter, r *http.Request, o OpenIDProvider) {
 	ctx, span := tracer.Start(r.Context(), "clientDelete")
 	r = r.WithContext(ctx)
 	defer span.End()
 
 	storage, err := assertClientStorage(o.Storage())
 	if err != nil {
-		return err
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	req, err := ParseClientDeleteRequest(r, o)
 	if err != nil {
-		// TODO(mqf20): be able to return the proper error codes?
-		return err
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	registrationAccessToken, err := getBearerToken(r)
 	if err != nil {
-		return err
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	if err := storage.AuthorizeClientDelete(ctx, req.ClientID, registrationAccessToken); err != nil {
-		return err
+		if errors.Is(err, ErrInvalidClient) || errors.Is(err, ErrInvalidRegistrationAccessToken) {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		if errors.Is(err, ErrClientNoPermission) {
+			http.Error(w, err.Error(), http.StatusForbidden)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	if err := storage.DeleteClient(ctx, req.ClientID); err != nil {
-		// TODO(mqf20): be able to return the proper error codes?
-		return err
+		if errors.Is(err, ErrInvalidClient) {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		if errors.Is(err, ErrClientDeleteNotSupported) {
+			http.Error(w, err.Error(), http.StatusMethodNotAllowed)
+			return
+		}
+		if errors.Is(err, ErrClientDeleteNotAllowed) {
+			http.Error(w, err.Error(), http.StatusForbidden)
+			return
+		}
+		o.Logger().Log(r.Context(), slog.LevelError, "delete client error", "error", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	w.WriteHeader(http.StatusNoContent)
-	return nil
+	return
 }
 
 func ParseClientDeleteRequest(r *http.Request, o OpenIDProvider) (*oidc.ClientDeleteRequest, error) {
